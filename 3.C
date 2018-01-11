976547300d8503febceb1b6e441bf9cf84ddce5a
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-01-11 16:08:52 +0530

    Create 3.C

---
+++ 3.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int lcase,ucase;
+printf("Enter the alphabet");
+scanf("%c",&c);
+lcase=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+ucase=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U'):
+if(lcase||ucase)
+ printf(" %c it is vowel",c);
+else
+ printf("%c is consonant",c);
+ 
+ return 0;
+ }
+ 
+ 
