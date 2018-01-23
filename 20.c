a766bf1b8224f17e0aaab7e78d876be798c58952
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-01-23 16:42:42 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num,mul;
+printf("enter the number\n");
+scanf("%d",&num);
+printf("multiplies are ");
+for(mul=1;mul<=25;mul++)
+{
+if(imul%5==0)
+printf("%d\n",mul);
+}
+return 0;
+}
