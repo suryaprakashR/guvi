465c5515b5c92100c2c1387685469cf3e1d8147f
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-01-23 16:15:51 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int number, originalNumber, remainder, result = 0, n = 0 ;
+printf("check armstrong or not\n");
+printf("Enter an integer: ");
+scanf("%d", &number);
+originalNumber = number;
+while (originalNumber != 0)
+{
+originalNumber /= 10;
+++n;
+}
+originalNumber = number;
+while (originalNumber != 0)
+{
+remainder = originalNumber%10;
+result += pow(remainder, n);
+originalNumber /= 10;
+}
+if(result == number)
+printf("yes");
+else
+printf("no");
+return 0;
+}
