513a10583ebbe7a7868593c7b0eacdbe691490b2
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-02-06 09:17:18 +0000

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int b,i,count=0;
+    printf("Enter the string:");
+    char a[1000];
+    gets(a);
+    b=strlen(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' ')
+        count++;
+    }
+    printf("the no of words in a element %d",b-count);
+}
