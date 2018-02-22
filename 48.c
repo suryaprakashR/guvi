5b221c6d88ba5ffd944a4a1f902e19ec5126a76c
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-02-22 15:58:38 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ void main()
+{
+  int i,n,Sum=0,num;
+  float Avg;
+  printf("\nPlease Enter How many Number you want?\n");
+  scanf("%d",&n);
+  printf("\nPlease Enter the elements one by one\n");
+  for(i=0;i<n;i++)
+   {
+     scanf("%d",&num);
+     Sum=Sum+num;
+   }
+  Avg=Sum/n;
+  printf("\nSum of the %d Numbers = %d",n, Sum);
+  printf("\nAverage of the %d Numbers = %.2f",n, Avg);
+  return 0;
+}
