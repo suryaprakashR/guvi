e02708245c225b4a7fe8d4dc5814c8dc07de47a9
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-02-22 16:08:09 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+ int main()
+{
+int a,b,sum;
+printf("Enter the value of a:");
+scanf("%d",&a);
+printf("Enter the value of b:");
+scanf("%d",&b);
+sum=a+b;
+printf("\n%d",sum);
+return 0;    
+}
