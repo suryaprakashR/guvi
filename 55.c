fe5c7e74bd26bbdb3a29918fd5b8ddd58b185afc
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-02-27 16:21:35 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,p;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    p=m*n;
+    if(n%2==0)
+    printf("even");
+    else
+    printf("odd");
+    return 0;
+}
