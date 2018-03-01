71571a057eb8082df26a6bc2409b09940a00df3d
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-03-01 16:05:51 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char a[1000];
+    int n,i;
+    scanf("%s",&a[i]);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%c",a[i]);
+    }
+    return 0;
+}
