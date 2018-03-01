71e465f8bf38b1d1491866690c3953d21bd42335
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-03-01 16:48:08 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,count=1;
+	char str[30];
+	scanf("%[^\n]s",&str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+return 0;
+}
