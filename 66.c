9fc1d24daa1a9a327d8186bc1a5f7d3fb8fd6496
suryaprakashR <30828835+suryaprakashR@users.noreply.github.com>
2018-03-02 16:25:16 +0530

    Update 66.c

--- 66.c
+++ 66.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,flag;
+    int i,n,flag=0;
     printf("enter a number:");
     scanf("%d",&n);
-    for(i=2;i<=n/2;i++)
+    for(i=2;i<=n/2;++i)
     {
         if(n%i==0)
          flag=1;
-         else
-         flag=0;
+       
     
     }
     if(flag==0)
